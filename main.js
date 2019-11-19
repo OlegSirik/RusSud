32819a84fd0bffd8b0fabe01bc73025a46fbd8e8
OlegSirik <oleg.sirik@mail.ru>
2019-11-19 10:25:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ app.get('/v1/card/:id/', function (req, res) {
 
 app.get('/v1/card/:id/purchases', function (req, res) { 
  
- res.json({[]});
+ res.json([{}]);
 });
 
 app.post('/callback/answerLogin', function (req, res) { 
